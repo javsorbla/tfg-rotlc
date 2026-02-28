5595410a0e95b39c2a4266f1cb801568b4d9da21
Javier Soria Blanco <javsorbla@alum.us.es>
2026-02-28 22:01:24 +0100

    feat: añadir proyecto base de godot

---
+++ Juego/player/player.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+const DOUBLE_JUMP_VELOCITY = -350.0
+
+var can_double_jump = false
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Salto
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			can_double_jump = true
+		elif can_double_jump:
+			velocity.y = DOUBLE_JUMP_VELOCITY
+			can_double_jump = false
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	_update_animation()
+
+func _update_animation():
+	if not is_on_floor():
+		# Aquí pondremos jump/fall cuando tengas esos sprites
+		pass
+	elif velocity.x != 0:
+		$AnimatedSprite2D.play("walk")
+	else:
+		$AnimatedSprite2D.play("idle")
+	
+	# Voltear el sprite según dirección
+	if velocity.x > 0:
+		$AnimatedSprite2D.flip_h = false
+	elif velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
