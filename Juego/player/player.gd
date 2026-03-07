9106e26b2d8429ad781a3c5d7e64e82126be4f13
Javier Soria Blanco <javsorbla@alum.us.es>
2026-03-07 20:32:36 +0100

    feat: añadir ataque básico

--- Juego/player/player.gd
+++ Juego/player/player.gd
@@ -1,11 +1,18 @@
 extends CharacterBody2D
 
+# Constantes de movimiento
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 const DASH_SPEED = 300.0
 const DASH_DURATION = 0.20
 const DASH_COOLDOWN = 0.5
 
+# Constantes de ataque
+const ATTACK_DURATION = 0.3
+const HITBOX_OFFSET_X = 14
+const HITBOX_OFFSET_Y = 22
+
+# Variables de movimiento
 var can_double_jump = false
 var was_on_floor = false
 var is_dashing = false
@@ -15,6 +22,12 @@ var dash_direction = 1.0
 var dash_cooldown_timer = 0.0
 var air_dash_used = false
 
+# Variables de ataque
+var is_attacking = false
+var attack_timer = 0.0
+var last_direction = 1
+@onready var hitbox = $AttackHitbox
+
 func _physics_process(delta: float) -> void:
 	
 	if dash_cooldown_timer > 0:
@@ -23,7 +36,7 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		air_dash_used = false
 	
-	var can_dash = dash_cooldown_timer <= 0 and (is_on_floor() or not air_dash_used)
+	can_dash = dash_cooldown_timer <= 0 and (is_on_floor() or not air_dash_used)
 		
 	if is_dashing:
 		dash_timer -= delta
@@ -73,11 +86,13 @@ func _physics_process(delta: float) -> void:
 
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
+		last_direction = direction
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED * 3)
+		velocity.x = move_toward(velocity.x, 0, SPEED * 0.2)
 
 	move_and_slide()
+	_handle_attack(delta)
 	_update_animation()
 
 func _update_animation():
@@ -94,3 +109,29 @@ func _update_animation():
 		$AnimatedSprite2D.flip_h = false
 	elif velocity.x < 0:
 		$AnimatedSprite2D.flip_h = true
+
+func _handle_attack(delta):
+	if is_attacking:
+		attack_timer -= delta
+		if attack_timer <= 0:
+			is_attacking = false
+			hitbox.monitoring = false
+			hitbox.visible = false  # quitamos esto cuando haya sprite
+		return  # no permite atacar mientras ya está atacando
+
+	if Input.is_action_just_pressed("attack"):
+		is_attacking = true
+		attack_timer = ATTACK_DURATION
+		hitbox.monitoring = true
+		hitbox.visible = true
+		if Input.is_action_pressed("aim_up"):
+			hitbox.position = Vector2(0, -HITBOX_OFFSET_Y)
+		elif Input.is_action_pressed("aim_down"):
+			hitbox.position = Vector2(0, HITBOX_OFFSET_Y)
+		elif Input.is_action_pressed("aim_left"):
+			hitbox.position = Vector2(-HITBOX_OFFSET_X, 0)
+			print("hitbox position: ", hitbox.position)
+		elif Input.is_action_pressed("aim_right"):
+			hitbox.position = Vector2(HITBOX_OFFSET_X, 0)
+		else:
+			hitbox.position = Vector2(HITBOX_OFFSET_X * last_direction, 0)
