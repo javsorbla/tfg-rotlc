2914f9d46721f05f2286658da64524ea617f823d
Javier Castilla Rodríguez <javcasrod1@alum.us.es>
2026-03-14 22:58:14 +0100

    feature: añadir colisión con los muros y estado de muerte del Núcleo Inestable

--- Juego/enemies/common/acechador_aereo/acechador_aereo.gd
+++ Juego/enemies/common/acechador_aereo/acechador_aereo.gd
@@ -39,7 +39,7 @@ func _ready() -> void:
 	current_health = MAX_HEALTH
 	player = get_tree().get_first_node_in_group("player")
 
-	$EnemyHitbox.body_entered.connect(_on_enemy_hitbox_area_entered)
+	$EnemyHitbox.area_entered.connect(_on_enemy_hitbox_area_entered)
 	$EnemyHurtbox.area_entered.connect(_on_enemy_hurtbox_area_entered)
 
 	patrol_origin = global_position
