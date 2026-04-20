213e48bc8a234a2b89cb8c824425590600147f4a
alereyper <alereyper@alum.us.es>
2026-04-20 03:04:50 +0200

    feat: mejorar emboscada

--- Juego/scenes/montañas_de_ceniza.gd
+++ Juego/scenes/montañas_de_ceniza.gd
@@ -2,25 +2,41 @@ extends Node2D
 
 @onready var tilemap = $Nivel 
 
-# 1. Guardamos una lista con la ruta exacta de los enemigos que queramos.
-# Fíjate que usamos $Enemigos/NombreDelEnemigo para llegar a ellos.
+# 1. Los enemigos de la emboscada
 @onready var enemigos_emboscada = [
 	$Enemigos/NucleoInestable2,
 	$Enemigos/NucleoInestable3
 ]
 
+# 2. La puerta que se abrirá al ganar (destruir)
 var bloques_a_destruir = [
 	Vector2i(300, 35), 
 	Vector2i(300, 36),
 	Vector2i(300, 37)
 ] 
 
+# 3. La puerta que se cerrará a tu espalda (crear)
+var bloques_a_crear = [
+	Vector2i(273, 35), 
+	Vector2i(273, 36),
+	Vector2i(273, 37)
+]
+var id_tileset = 1 
+var coordenadas_imagen = Vector2i(2, 1) 
+
 
 func _ready() -> void:
-	# 2. Usamos un bucle para congelar SOLO a los de la lista
+	# Congelamos a los enemigos
 	for enemigo in enemigos_emboscada:
 		enemigo.visible = false
 		enemigo.process_mode = Node.PROCESS_MODE_DISABLED
+		
+	# --- ¡NUEVO! ---
+	# Borramos los bloques de la entrada que dibujaste en el editor 
+	# para que el camino esté abierto al empezar a jugar.
+	for posicion in bloques_a_crear:
+		tilemap.erase_cell(posicion)
+	# ---------------
 
 
 func _process(delta: float) -> void:
@@ -38,7 +54,12 @@ func _on_trigger_supervivencia_body_entered(body):
 	if body.name == "Player":
 		$Timer.start() 
 		
-		# 3. Despertamos SOLO a los de la lista
+		# CREAMOS EL MURO A LA ESPALDA
+		for posicion in bloques_a_crear:
+			tilemap.set_cell(posicion, id_tileset, coordenadas_imagen)
+		print("🚪 ¡Muro de entrada creado! No hay escapatoria.")
+		
+		# DESPERTAMOS ENEMIGOS
 		for enemigo in enemigos_emboscada:
 			enemigo.visible = true
 			enemigo.process_mode = Node.PROCESS_MODE_INHERIT
