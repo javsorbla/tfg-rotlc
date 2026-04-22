5e3a4ec333441e59e804be118fa8c1abc6ace9af
Javier Castilla Rodríguez <javcasrod1@alum.us.es>
2026-04-22 22:41:07 +0200

    feat: añadir transición entre los niveles 1 y 2

--- Juego/scenes/montañas_de_ceniza.gd
+++ Juego/scenes/montañas_de_ceniza.gd
@@ -1,21 +1,17 @@
 extends Node2D
 
 @onready var tilemap = $Nivel 
-
-# 1. Los enemigos de la emboscada
 @onready var enemigos_emboscada = [
 	$Enemigos/NucleoInestable2,
 	$Enemigos/NucleoInestable3
 ]
 
-# 2. La puerta que se abrirá al ganar (destruir)
 var bloques_a_destruir = [
 	Vector2i(300, 35), 
 	Vector2i(300, 36),
 	Vector2i(300, 37)
 ] 
 
-# 3. La puerta que se cerrará a tu espalda (crear)
 var bloques_a_crear = [
 	Vector2i(273, 35), 
 	Vector2i(273, 36),
@@ -26,52 +22,59 @@ var coordenadas_imagen = Vector2i(2, 1)
 
 
 func _ready() -> void:
-	# Congelamos a los enemigos
+	Hud.show_hud()
+	GameState.current_level = 2
+	call_deferred("_mover_player")
+	
 	for enemigo in enemigos_emboscada:
 		enemigo.visible = false
 		enemigo.process_mode = Node.PROCESS_MODE_DISABLED
-		
-	# --- ¡NUEVO! ---
-	# Borramos los bloques de la entrada que dibujaste en el editor 
-	# para que el camino esté abierto al empezar a jugar.
+
 	for posicion in bloques_a_crear:
 		tilemap.erase_cell(posicion)
-	# ---------------
 
 
 func _process(delta: float) -> void:
 	pass
 
 
-# --- CÓDIGO PARA REINICIAR LA ESCENA ---
 func _unhandled_input(event):
 	if event.is_action_pressed("reiniciar_escena"):
 		get_tree().reload_current_scene()
 
 
-# --- CÓDIGO PARA EL TRIGGER ---
 func _on_trigger_supervivencia_body_entered(body):
 	if body.name == "Player":
 		$Timer.start() 
 		
-		# CREAMOS EL MURO A LA ESPALDA
 		for posicion in bloques_a_crear:
 			tilemap.set_cell(posicion, id_tileset, coordenadas_imagen)
-		print("🚪 ¡Muro de entrada creado! No hay escapatoria.")
 		
-		# DESPERTAMOS ENEMIGOS
 		for enemigo in enemigos_emboscada:
 			enemigo.visible = true
 			enemigo.process_mode = Node.PROCESS_MODE_INHERIT
 			
-		print("😈 ¡Emboscada! Enemigos específicos activados.")
 		
 		$TriggerSupervivencia.queue_free()
 
 
-# --- CÓDIGO PARA DESTRUIR MÚLTIPLES BLOQUES ---
 func _on_timer_timeout():
 	for posicion in bloques_a_destruir:
 		tilemap.erase_cell(posicion)
 		
-	print("🧱 ¡Camino abierto!")
+	
+func _mover_player() -> void:
+	var player = get_tree().get_first_node_in_group("player")
+	if player:
+		var player_health = player.get_node("Health")
+		Hud.show_hud()
+		Hud.update_hearts(player_health.current_health, player_health.MAX_HEALTH)
+		if GameState.coming_from_transition:
+			GameState.coming_from_transition = false
+			GameState.checkpoint_activated = false
+			player.global_position = Vector2(-78, 18)
+		elif GameState.checkpoint_activated:
+			player.global_position = GameState.spawn_position
+		else:
+			player.global_position = Vector2(-78, 18)
+	
