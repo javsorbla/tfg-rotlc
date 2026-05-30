1d3b3b2b25862f58cdf19ae46e16c9e8facd71ab
Javier Castilla Rodríguez <javcasrod1@alum.us.es>
2026-05-30 19:39:18 +0200

    feat: añadir iluminación general del nivel 3

---
+++ Juego/scenes/zona_cueva_2.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var color_zona: Color = Color("#666666")
+var canvas_modulate: CanvasModulate
+
+func _ready():
+	canvas_modulate = get_tree().get_first_node_in_group("canvas_modulate")
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		canvas_modulate.cambiar_zona(color_zona)
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		canvas_modulate.cambiar_zona(Color("#aaaaaa"))
